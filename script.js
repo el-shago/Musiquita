bffbf88f9084953bd8d1b0f1f80ccba26c7f86f5
el-shago <sperezmaris@gmail.com>
2023-01-02 11:52:56 -0800

    Listillo

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let progress = document.getElementById("progress");
 let song = document.getElementById("song");
 let ctrlIcon = document.getElementById("ctrlIcon");
 
+
 song.onloadedmetadata = function(){
   progress.max = song.duration;
   progress.value = song.currentTime;
@@ -15,7 +16,20 @@ function playPause(){
   }
   else{
     song.play();
-    ctrlIcon.classList.remove("fa-pause");
-    ctrlIcon.classList.add("fa-play");
+    ctrlIcon.classList.add("fa-pause");
+    ctrlIcon.classList.remove("fa-play");
   }
+}
+
+if(song.play){
+  setInterval(()=>{
+    progress.value = song.currentTime;
+  },500);
+}
+
+progress.onchange = function(){
+  song.play();
+  song.currentTime = progress.value;
+  ctrlIcon.classList.add("fa-pause");
+  ctrlIcon.classList.remove("fa-play");
 }
